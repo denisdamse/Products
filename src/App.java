954aa9a53078ba36d757569ccb40494741af11fe
Denis <denisdamse@gmail.com>
2024-04-06 11:22:24 +0300

    ds

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) {
         Product.constructProduct("001","Rosii",32,0);
-        
     }
 }
--- src/Warehouse.java
+++ src/Warehouse.java
@@ -1,3 +1,4 @@
 public class Warehouse {
+    public static int numberOfProducts=0;
     
 }
